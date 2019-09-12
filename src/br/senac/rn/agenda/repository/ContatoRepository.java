ea57d322b6e357f52d0cdc55bb2f2c6e5068079a
Weskley Bezerra <weskleymb@hotmail.com>
2019-09-12 20:10:12 -0300

    primeiro commit

--- src/br/senac/rn/agenda/repository/ContatoRepository.java
+++ src/br/senac/rn/agenda/repository/ContatoRepository.java
@@ -12,10 +12,10 @@ public class ContatoRepository implements DataBase<Contato> {
 
     @Override
     public List<Contato> findAll() {
-        db.openConnection();
         List<Contato> contatos = new ArrayList();
-        String sql = "SELECT * FROM tb_contatos";
         try {
+            db.openConnection();
+            String sql = "SELECT * FROM tb_contatos";
             PreparedStatement ps = db.getConnection().prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
@@ -25,18 +25,34 @@ public class ContatoRepository implements DataBase<Contato> {
                 contato.setFone(rs.getString("con_fone"));
                 contatos.add(contato);
             }
+        } catch(SQLException error) {
+            System.out.println("ERRO: " + error);
+        } finally {
             db.closeConnection();
             return contatos;
-        } catch (SQLException error) {
-            System.out.println("Erro: " + error);
         }
-        db.closeConnection();
-        return null;
     }
 
     @Override
     public Contato findById(Long id) {
-        return null;
+        Contato contato = new Contato();
+        try {
+            db.openConnection();
+            String sql = "SELECT * FROM tb_contatos WHERE con_id = ?";
+            PreparedStatement ps = db.getConnection().prepareStatement(sql);
+            ps.setLong(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                contato.setId(rs.getLong("con_id"));
+                contato.setNome(rs.getString("con_nome"));
+                contato.setFone(rs.getString("con_fone"));
+            }
+        } catch (SQLException error) {
+            System.out.println("ERRO: " + error);
+        } finally {
+            db.closeConnection();
+            return contato;
+        }
     }
 
     @Override
